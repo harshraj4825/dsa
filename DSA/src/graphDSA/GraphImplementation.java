e33e0b42fe22bb4079617ccf148be7527cef6fe8
harshraj4825 <iitianharshraj@gmail.com>
2021-04-21 12:41:56 +0530

    Implementation of Adjacency Matrix of weighted Graph console output Adjacency of Matrix of un_weighted Graph Graph 0 1 0 1 1 0 1 1 0 1 0 1 1 1 0 0
    After removing
    0 1 0 1
    1 0 0 1
    0 0 0 0
    1 1 0 0
    
    Adjacency of Matrix of weighted Graph Graph
    0 1 0 3
    1 0 4 2
    0 4 0 1
    3 2 0 0
    
    After removing
    0 1 0 3
    1 0 0 2
    0 0 0 0
    3 2 0 0

--- DSA/src/graphDSA/GraphImplementation.java
+++ DSA/src/graphDSA/GraphImplementation.java
@@ -3,7 +3,7 @@ package graphDSA;
 public class GraphImplementation {
 
 	public static void main(String[] args) {
-		//Adjacency of Matrix of unweighted Graph Graph
+		//Adjacency of un_weighted Graph
 		AdjacencyMatrixGraph graph=new AdjacencyMatrixGraph(4);
 		
 		graph.addEdge(0, 1, false);
@@ -17,6 +17,21 @@ public class GraphImplementation {
 		graph.removeEdge(1, 2, false);
 		graph.removeEdge(2, 3, true);
 		System.out.println(graph.toString());
+		
+		//Adjacency Matrix of weighted Graph
+		WeightedGraphAM W_graph=new WeightedGraphAM(4);
+		
+		W_graph.addEdge(0, 1, false,1);
+		W_graph.addEdge(0, 3, false,3);
+		W_graph.addEdge(1, 2, false,4);
+		W_graph.addEdge(2, 3, true,1);
+		W_graph.addEdge(1, 3, false,2);
+		System.out.println("Adjacency of Matrix of weighted Graph Graph");
+		System.out.println(W_graph.toString());
+		System.out.println("After removing");
+		W_graph.removeEdge(1, 2, false);
+		W_graph.removeEdge(2, 3, true);
+		System.out.println(W_graph.toString());
 	}
 	
 	
---
+++ DSA/src/graphDSA/WeightedGraphAM.java
@@ -0,0 +1,36 @@
+package graphDSA;
+// Adjacency matrix of weighted graph
+public class WeightedGraphAM {
+	int V;
+	int[][] G;
+	public WeightedGraphAM(int noOfvertices) {
+		this.V=noOfvertices;
+		this.G=new int[V][V];
+	}
+	//add edge
+	void addEdge(int source,int destination,boolean directed,int weight) {
+		G[source][destination]=weight;
+		if(!directed) {//if undirected graph then add destination to source
+			G[destination][source]=weight;
+		}
+	}
+	//remove edge
+	void removeEdge(int source,int destination,boolean directed) {
+		G[source][destination]=0;
+		if(!directed) {//if directed then don't remove
+			G[destination][source]=0;
+		}
+	}
+	//print Graph
+	 public String toString() {
+		    StringBuilder s = new StringBuilder();
+		    for (int i = 0; i < V; i++) {
+		      for (int j : G[i]) {
+		        s.append(j + " ");
+		      }
+		      s.append("\n");
+		    }
+		  return s.toString();
+	}
+
+}
