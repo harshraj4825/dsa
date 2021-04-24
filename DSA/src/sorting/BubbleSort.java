498e5f6137d7e62c898df95dd6ec8f837170c37e
harshraj4825 <iitianharshraj@gmail.com>
2021-04-24 20:31:23 +0530

    Bubble sort algorithm

---
+++ DSA/src/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package sorting;
+
+import java.util.Arrays;
+
+	/*Principle for ascending:- compare first and second elements. if e1>e2 than swap. Apply loop
+	 * then you will get last element 
+	 */
+public class BubbleSort {
+	void bubblesort(int arr[]) {
+		int size=arr.length;
+		for(int i=0;i<size;i++) {
+			for(int j=0;j<size-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					arr[j]=arr[j]^arr[j+1];
+					arr[j+1]=arr[j]^arr[j+1];
+					arr[j]=arr[j]^arr[j+1];
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int data[]= {-2,45,0,11,-9};
+		BubbleSort bs=new BubbleSort();
+		bs.bubblesort(data);
+		System.out.println(Arrays.toString(data));
+		
+	}
+
+}
