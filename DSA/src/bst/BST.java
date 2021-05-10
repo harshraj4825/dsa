fc2ccb4ad17c545c49b45f583a38e4668eb61299
harshraj4825 <iitianharshraj@gmail.com>
2021-05-10 18:30:49 +0530

    BST update

--- DSA/src/bst/BST.java
+++ DSA/src/bst/BST.java
@@ -1,55 +1,49 @@
 package bst;
-
+import helperUtil.Node;
 public class BST {
-	class Node{
-		int val;
-		Node left,right;
-		public Node(int val) {
-			this.val=val;
-			left=null;
-			right=null;
-		}
-	}
+//	class Node{
+//		
+//	}
 	//search in BST
 	boolean searchBST(Node root,int val) {
 		if(root==null)return false;
-		if(root.val==val)return true;
-		else if(val<root.val)return searchBST(root.left,val);
+		if(root.data==val)return true;
+		else if(val<root.data)return searchBST(root.left,val);
 		else return searchBST(root.right,val);
 	}
 	//Searching maximum in BST
 	int maxBST(Node root) {
 		if(root==null)return -1;
-		if(root.left==null&&root.right==null)return root.val;
+		if(root.left==null&&root.right==null)return root.data;
 		else if(root.right!=null)return maxBST(root.right);
 		else return maxBST(root.left);
 	}
 	//Searching minimum in BST
 	int minBST(Node root) {
 		if(root==null)return -1;
-		if(root.left==null&&root.right==null)return root.val;
+		if(root.left==null&&root.right==null)return root.data;
 		else if(root.left!=null)return minBST(root.left);
 		else return minBST(root.right);
 	}
 	//insert in BST
 	Node insert(Node root,int val) {
 		if(root==null) return new Node(val);
-		if(val==root.val)return root;
-		if(val<root.val)root.left=insert(root.left,val);
-		if(val>root.val)root.right=insert(root.right,val);
+		if(val==root.data)return root;
+		if(val<root.data)root.left=insert(root.left,val);
+		if(val>root.data)root.right=insert(root.right,val);
 		return root;
 	}
 	//Deletion in BST
 	Node deleteNode(Node root,int val) {
 		if(root==null)return null;
-		if(root.val==val) {
+		if(root.data==val) {
 			if(root.left==null && root.right==null) {
 				root=null;
 			}
 			else if(root.left!=null && root.right!=null) {
 				Node temp=util(root.left);
-				root.val=temp.val;
-				root.left=deleteNode(root.left,temp.val);
+				root.data=temp.data;
+				root.left=deleteNode(root.left,temp.data);
 				return root;
 			}
 			else if(root.left!=null) {
@@ -59,7 +53,7 @@ public class BST {
 				return root.right;
 			}
 		}
-		else if(val<root.val) {
+		else if(val<root.data) {
 			root.left=deleteNode(root.left,val);
 		}else {
 			root.right=deleteNode(root.right,val);
@@ -82,7 +76,7 @@ public class BST {
 	void inOrederHelper(Node root,StringBuilder sb) {
 		if(root==null)return ;
 		inOrederHelper(root.left,sb);
-		sb.append(root.val+" ");
+		sb.append(root.data+" ");
 		inOrederHelper(root.right,sb);	
 	}
 }
